7dc1d9e91d802c5c3745382e31acfa373317b7b1
Clément DOUIN <clement.douin@gmail.com>
2018-09-24 03:15:21 +0200

    parse mails from INBOX

--- iris.py
+++ iris.py
@@ -1,6 +1,12 @@
-import vim
+#!/usr/bin/env python3
 
-from datetime import date
+import re
+import vim
+import urllib
+import quopri
+
+from datetime import date, datetime
+from functools import reduce
 from imapclient.imapclient import IMAPClient
 
 def imap_connect():
@@ -13,7 +19,23 @@ def imap_connect():
     client.select_folder('INBOX', readonly=True)
 
     sort = client.search(['NOT', 'DELETED', 'SINCE', date(2018, 9, 1)])
-    response = client.fetch(sort, ['FLAGS', 'RFC822.SIZE'])
+    response = client.fetch(sort, ['BODY.PEEK[HEADER]', 'INTERNALDATE'])
     client.logout()
 
-    return response.items()
+    messages = []
+
+    for [id, data] in response.items():
+        headers = {}
+        matches = re.findall('(.*?): (.*?)\r\n', data[b'BODY[HEADER]'].decode())
+
+        for match in matches:
+            headers[match[0]] = match[1]
+
+        try:
+            subject = quopri.decodestring(headers['Subject']).decode()
+        except:
+            subject = urllib.parse.unquote(headers['Subject'])
+
+        messages.append(' - '.join([str(id), subject, headers['From'], data[b'INTERNALDATE'].strftime('%x')]))
+
+    return messages
--- plugin/iris.vim
+++ plugin/iris.vim
@@ -4,6 +4,19 @@ let s:script = resolve(s:root_dir . '/iris.py')
 let g:iris_host  = get(g:, 'iris_host', '')
 let g:iris_email = get(g:, 'iris_email', '')
 
+let s:const = {
+  \'INBOX': {
+    \'column': ['id', 'from', 'subject', 'date', 'flags'],
+    \'width': {
+      \'id': 5,
+      \'from': 26,
+      \'subject': 29,
+      \'date': 16,
+      \'flags': 4,
+    \},
+  \},
+\}
+
 function! IrisConnect()
   let password = inputsecret(
     \'Iris password :' .
@@ -11,10 +24,10 @@ function! IrisConnect()
   \)
 
   redraw
-  execute 'python import sys; sys.path.insert(0, "'.s:root_dir.'/imapclient")'
-  execute 'pyfile ' . s:script
+  execute 'python3 import sys; sys.path.insert(0, "'.s:root_dir.'/imapclient")'
+  execute 'py3file ' . s:script
 
-  let messages = map(pyeval('imap_connect()'), 'v:val[0] . " [" . join(v:val[1]["FLAGS"], ", ") . "]"')
+  let messages = py3eval('imap_connect()')
 
   silent! edit Iris
   call append(0, messages)
